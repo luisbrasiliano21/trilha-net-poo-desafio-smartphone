c518e5a880791ad5c525e3dcd185d9a441afd0cd
luisbrasiliano21 <luiskevinibn@gmail.com>
2023-12-24 11:30:27 -0300

    Desafio de C# Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,30 @@
+using System.Runtime.ConstrainedExecution;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        //Feito
+        private string Modelo;
 
-        public Smartphone(string numero)
+        private string Imei;
+
+        private int Memoria;
+
+
+
+        public Smartphone(string numero,string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+
+            Imei = imei;
+
+            Memoria = memoria;
+
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//Feito
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"123456", modelo:"Nokia 30", imei: "546738", memoria: 120  );
+nokia.Ligar();
+nokia.InstalarAplicativo("\"Whatsapp\"");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"234567", modelo: "iphone 15", imei:"1111111111", memoria:180);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("\"Telegram\"");
